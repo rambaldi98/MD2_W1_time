b08306a4c3ad31413dcffcfad9d66e6626db7379
rambaldi <diencong5998@gmail.com>
2021-04-02 11:47:28 +0700

    hello name

---
+++ src/HelloName.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloName {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println(name);
+    }
+}
