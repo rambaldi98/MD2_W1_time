44669ca2dc760bd7960ee67d3b2d867598abdb83
rambaldi <diencong5998@gmail.com>
2021-04-02 10:36:29 +0700

    area

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Demo1 {
+    public static void main(String[] args) {
+            float height ;
+            float width;
+        Scanner s = new Scanner(System.in);
+        height = s.nextFloat();
+        width = s.nextFloat();
+        float area = height * width;
+        System.out.print("area = "+area );
+    }
 }
